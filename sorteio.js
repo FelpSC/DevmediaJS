90777a7cf17220ba152cd851ae49b754dc461372
FelpSC <felipetoral1998@gmail.com>
2024-08-30 13:40:34 -0300

    Criando um sorteio

---
+++ sorteio.js
@@ -0,0 +1,14 @@
+let numeros = [ 1,2,3,4,5,6,7,8,9,10];
+let numeroRandom = numeros.sort(() => Math.random() - 0.5);
+let numeroSorteado = numeroRandom;
+
+// function sortearNumero(numero){
+//     var sortear = 
+//     addEventListener
+    
+// }
+
+
+numeros = numeros.splice(numeroSorteado);
+console.log(numeros);
+console.log(numeroSorteado);
