1acbca28cc6e41ea3938ecd2c403c305dc93a87e
FelpSC <felipetoral1998@gmail.com>
2024-09-10 02:07:00 -0300

    Update exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -1,6 +1,35 @@
+let series = [
+  {nome: 'Loki', servico: 'Disney+'},
+  {nome: 'Friends', servico: 'HBO'},
+  {nome: 'Superman & Lois', servico: 'HBO'},
+  {nome: 'WandaVision', servico: 'Disney+'},
+];
 
-const cores = [ 'Verde', 'Amarelo', 'Azul', 'Branco' ]
 
-for (const index in cores) {
-  console.log(index, cores[index])
+function retornaSeriesDisney(serie){
+	return ( serie == 'Disney+')
+
 }
+
+function retornaSeriesHBO(serie){
+	return (serie == 'HBO');
+}
+
+
+let servico = 'Disney';
+let retornaSeries = [];
+
+switch(servico){
+    
+  case'Disney':
+          retornaSeries = series.filter(serie => serie.servico === 'Disney+');
+          break;
+      
+  case'HBO':
+          retornaSeries = series.filter(serie => serie.servico === 'HBO');
+          break;
+}
+
+retornaSeries.forEach(serie => {
+	console.log(serie.nome);
+});
